36e71e07bfe27d3f499759c195e1004701542477
NikuBaguette <81246303+NikuBaguette@users.noreply.github.com>
2023-05-19 16:49:34 +0200

    Update runapp.py

--- Notre Site/site_officiel/runapp.py
+++ Notre Site/site_officiel/runapp.py
@@ -9,11 +9,14 @@ def index():
     if path.exists("templates\\index.html") != False :
         return render_template("index.html")
     else:
-        return "La page d'index s'affichera lorsqu'elle sera renseigné dans le dossier templates"
+        return "La page d'index s'affichera lorsqu'elle sera renseigné dans le dossier templates \n sous le nom de index.html"
 
 @app.route("/result")
 
 def result():
-    return "in progress"
+    if path.exists("templates\\result.html") != False :
+        return render_template("result.html")
+    else:
+        return "La page de résultat s'affichera lorsqu'elle sera renseigné dans le dossier templates \n sous le nom de result.html"
 
-app.run( debug=True, host='0.0.0.0', port=5000 )
+app.run(debug=True, host='0.0.0.0', port=5000)
