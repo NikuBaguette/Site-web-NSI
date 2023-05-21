0eb4339ec4bfcaac0a6ae9030623a432f0823d77
NikuBaguette <81246303+NikuBaguette@users.noreply.github.com>
2023-05-21 18:57:34 +0200

    Add files via upload

--- Notre Site/site_officiel/data/sqlite_select.py
+++ Notre Site/site_officiel/data/sqlite_select.py
@@ -8,77 +8,28 @@ def find_min():
     conn.commit()
     minimum = cur.fetchall()[0]
     return  minimum
+
 def find_moyenne():
     cur.execute("Select Avg(note) From Jeux")
     conn.commit()
     moyenne = cur.fetchall()[0][0]
     return moyenne
+
 def find_maxi():
     cur.execute("Select Nom,max(note) From Jeux")
     conn.commit()
     maximum = cur.fetchall()[0]
     return maximum
-def find_Minecraft():
-    cur.execute("Select * from Jeux where ID_jeux=1")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
 
-def find_Valorant():
-    cur.execute("Select * from Jeux where ID_jeux=2")
+def find_Game(id: int):
+    cur.execute(f"Select * from Jeux where ID_jeux={id}")
     conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_Apex():
-    cur.execute("Select * from Jeux where ID_jeux=3")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_Roblox():
-    cur.execute("Select * from Jeux where ID_jeux=4")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_CSGO():
-    cur.execute("Select * from Jeux where ID_jeux=5")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_Smash_bros():
-    cur.execute("Select * from Jeux where ID_jeux=6")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_GTAV():
-    cur.execute("Select * from Jeux where ID_jeux=7")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_Fortnite():
-    cur.execute("Select * from Jeux where ID_jeux=8")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_Super_mario_odyssey():
-    cur.execute("Select * from Jeux where ID_jeux=9")
-    conn.commit()
-    result=cur.fetchall()[0]
-    return result
-
-def find_Cyberpunk():
-    cur.execute("Select * from Jeux where ID_jeux=10")
-    conn.commit()
-    result=cur.fetchall()[0]
+    result = cur.fetchall()[0]
     return result
 
 print(find_maxi())
 print(find_min())
 print(find_moyenne())
-print(find_Minecraft())
+
+for i in range(1, 11):
+    print(find_Game(i)) # je te ez Lucien
