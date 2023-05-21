9775ad0db04908898253ad870dcb24a38c024291
zelleea <133632440+zelleea@users.noreply.github.com>
2023-05-21 15:34:12 +0200

    Add files via upload

--- Notre Site/site_officiel/data/ouverture_base.py
+++ Notre Site/site_officiel/data/ouverture_base.py
@@ -2,11 +2,11 @@ import sqlite3
 from faker import Faker
 import random
 
-fake=Faker(local='fr_FR')
-con=sqlite3.connect('personne.db')
+fake = Faker(local='fr_FR')
+con = sqlite3.connect('personne.db')
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS Personne(ID_personne INT PRIMARY KEY,Nom TEXT,Email TEXT,Adresse TEXT,Ville TEXT,Code_postal INT,Note INT,ID_jeux INT)")
-cur.execute("CREATE TABLE IF NOT EXISTS Jeux(ID_Jeux INT PRIMARY KEY,Nom TEXT,Date_sortie TEXT,Note INT)")
+cur.execute("CREATE TABLE IF NOT EXISTS Personne(ID_personne INT PRIMARY KEY,Nom TEXT,Email TEXT,Adresse TEXT,Ville TEXT,Code_postal INT,Note INT,Avis_jeux TEXT,ID_jeux INT)")
+cur.execute("CREATE TABLE IF NOT EXISTS Jeux(ID_Jeux INT PRIMARY KEY,Nom TEXT,Date_sortie TEXT,Note INT,Avis_jeux TEXT)")
 
 sql = 'DELETE FROM Personne'
 sql1 = 'DELETE FROM Jeux'
@@ -16,24 +16,42 @@ con.commit()
 
 
 listejeux=[("minecraft","18 novembre 2011"),("valorant","2 juin 2020"),("Apex","4 février 2019"),("Roblox","1 septembre 2006"),("CSGO","21 août 2012"),("smash bros","7 décembre 2018"),("GTAV","17 septembre 2013"),("Fortnite","21 juillet 2017"),("super mario odyssey","27 octobre 2017"),("cyberpunk 2077","10 décembre 2020")]
+liste_avis=["Trop Nul","BOF","Bien","Tres bien"]
 data=[]
 data2=[]
 
 for i in range(40):
-      temp1=(i+1,fake.name(),fake.email(),fake.street_address(),fake.city(),fake.postcode(),random.randint(0,10),random.randint(1,10))
+      note=random.randint(0,10)
+      if note<=3:
+            avis=liste_avis[0]
+      elif note<=5:
+            avis=liste_avis[1]
+      elif note<=7:
+            avis=liste_avis[2]
+      else:
+            avis=liste_avis[3]
+      temp1=(i+1,fake.name(),fake.email(),fake.street_address(),fake.city(),fake.postcode(),note,avis,random.randint(1,10))
       data.append(temp1)
-cur.executemany("INSERT INTO Personne VALUES(?, ?, ?, ?, ?, ?, ?, ?)", data)
+cur.executemany("INSERT INTO Personne VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
 con.commit()
 
 for i in range(10):
       cur.execute(f"Select AVG(note) from Personne where id_jeux={i+1}")
       note=cur.fetchall()[0][0]
       if note == None:
-            note=0
-      note=round(note)
-      temp2 = (i+1, listejeux[i][0], listejeux[i][1], note)
+            note = 0
+      note = round(note)
+      if note <= 3:
+            avis = liste_avis[0]
+      elif note <= 5:
+            avis = liste_avis[1]
+      elif note <= 7:
+            avis = liste_avis[2]
+      else:
+            avis = liste_avis[3]
+      temp2 = (i+1, listejeux[i][0], listejeux[i][1], note,avis)
       data2.append(temp2)
-cur.executemany("INSERT INTO JEUX VALUES(?, ?, ? ,?)", data2)
+cur.executemany("INSERT INTO JEUX VALUES(?, ?, ? ,?, ?)", data2)
 con.commit()
 
 
--- Notre Site/site_officiel/data/sqlite_select.py
+++ Notre Site/site_officiel/data/sqlite_select.py
@@ -2,17 +2,20 @@ import sqlite3
 
 conn = sqlite3.connect("personne.db")
 cur = conn.cursor()
-
+cur.execute("Select count(nom) from Personne")
+conn.commit()
+nombre=cur.fetchall()
+print(nombre)
 def find():
     cur.execute("SELECT Nom,min(note) FROM Jeux")
     conn.commit()
-    minimum = cur.fetchall()
+    minimum = cur.fetchall()[0]
     cur.execute("Select Avg(note) From Jeux")
     conn.commit()
-    moyenne = cur.fetchall()
+    moyenne = cur.fetchall()[0][0]
     cur.execute("Select Nom,max(note) From Jeux")
     conn.commit()
-    maximum = cur.fetchall()
+    maximum = cur.fetchall()[0]
     cur.close()
     conn.close()
     return {"minimum": minimum, "maximum": maximum, "moyenne": moyenne}
